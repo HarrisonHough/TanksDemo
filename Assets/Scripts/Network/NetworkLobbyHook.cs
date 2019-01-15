b210d31e8c918c95511b67dc5ae9dde5c94e271c
Harrison Hough <harrisonhough4@gmail.com>
2019-01-15 20:30:31 +1100

    Rebuilding Networked multiplayer

--- Assets/Scripts/Network/NetworkLobbyHook.cs
+++ Assets/Scripts/Network/NetworkLobbyHook.cs
@@ -6,25 +6,5 @@ using UnityEngine.Networking;
 
 public class NetworkLobbyHook : LobbyHook
 {
-    public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
-    {
-        //base.OnLobbyServerSceneLoadedForPlayer(manager, lobbyPlayer, gamePlayer);
-
-        LobbyPlayer lPlayer = lobbyPlayer.GetComponent<LobbyPlayer>();
-
-        NetworkPlayerSetup playerSetup = gamePlayer.GetComponent<NetworkPlayerSetup>();
-
-        playerSetup.PlayerName = lPlayer.playerName;
-        playerSetup.PlayerColor = lPlayer.playerColor;
-
-
-        NetworkPlayerController pManager = gamePlayer.GetComponent<NetworkPlayerController>();
-        if (pManager != null)
-        {
-            NetworkGameManager.AllPlayers.Add(pManager);
-        }
-
-    }
-
-
+    
 }
